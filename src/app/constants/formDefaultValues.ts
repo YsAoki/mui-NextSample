3b624ad83f8db59657b1a7c7e9bd52672f3fe7d2
YsAoki <kazuaki.aoki@ysinc.co.jp>
2024-12-31 06:24:05 +0900

    constantsファイルにemptyとnotFoundの定数を追加

---
+++ src/app/constants/formDefaultValues.ts
@@ -0,0 +1,11 @@
+/** 入力値が空の場合 */
+export const EMPTY_INPUT = "";
+
+/** ラジオボタン、セレクトボックスが空の場合 */
+export const EMPTY_VALUE = "";
+
+/** チェックボックスが未選択状態の場合(length値の検証で使用) */
+export const EMPTY_CHECKBOX = 0;
+
+/** DatePicker の初期値（未選択の場合） */
+export const EMPTY_DATE = null;
---
+++ src/app/constants/notFound.ts
@@ -0,0 +1,5 @@
+/** 配列のfindメソッドが該当なしの場合 */
+export const NOT_FOUND_TARGET = undefined;
+
+/** 配列のえfind indexメソッドで該当なしの場合 */
+export const NOT_FOUND_INDEX = -1;
